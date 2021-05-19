91932b91e4fbeeee1c8b31e6d8f3f0e6434b7159
yen3k <gitlab@yenmail.party>
2021-05-19 16:32:17 +0200

    Isolate important part of sound

--- fft.py
+++ fft.py
@@ -7,15 +7,17 @@ def wav_fft(fp):
     print(f"Sound: {fp.split('/')[-1]}")
     rate, data = wavfile.read(fp)
 
-    avg_data = data.sum(axis=1) / 2
+    avg_data = data.sum(axis=1) // 2
+    sidx, eidx, cropped_data = crop_sound(avg_data, 1000, 5)
+    print(f"Analyzing from {sidx} to {eidx}")
 
-    N = avg_data.shape[0]
+    N = cropped_data.shape[0]
     s = N / float(rate)
     t_step = 1.0 / rate
 
     t = np.arange(0, s, t_step)
 
-    fft_data = abs(fft(avg_data))
+    fft_data = abs(fft(cropped_data))
     fft_data_half = fft_data[:N//2]
 
     f = fftfreq(N, t_step)
@@ -23,19 +25,37 @@ def wav_fft(fp):
 
     fig, (ax1, ax2) = plt.subplots(2)
     fig.suptitle(fp.split('/')[-1])
-    ax1.plot(t, avg_data, 'r', marker='o')
-    ax2.plot(f_half, fft_data_half, 'g', marker='o')
+    ax1.plot(t, cropped_data, 'r')
+    ax2.plot(f_half, fft_data_half, 'g')
     plt.savefig('figures/' + fp.split('/')[-1].split('.')[0])
     plt.show()
 
     f3 = argmax_n(fft_data_half, 3)
 
-    print(f"Most contributing frequencies: {f_half[f3]}")
+    print(f"Most contributing frequencies: {f_half[f3].astype('int')}")
     print()
-    return t, avg_data, f_half, fft_data_half
 
 def argmax_n(x, n):
     return np.flip(np.argsort(x)[-n:])
 
-t_10, data_10, f_10, fft_10 = wav_fft('audio/tube10.wav')
-t_20, data_20, f_20, fft_20 = wav_fft('audio/tube20.wav')
+def crop_sound(data, threshold, n):
+    start_idx = 0
+    end_ind = len(data)-1
+
+    # start index
+    for i in range(len(data)):
+        if abs(data[i]) > threshold and np.all(abs(data[i:i+n]) > threshold):
+            start_idx = i
+            break
+
+    # end index
+    for i in range(start_idx, len(data)):
+        if abs(data[i]) < threshold and np.all(abs(data[i:i+n]) < threshold):
+            end_idx = i
+            break
+
+    return start_idx, end_idx, data[start_idx:end_idx]
+
+if __name__ == '__main__':
+    wav_fft('audio/tube10.wav')
+    wav_fft('audio/tube20.wav')
