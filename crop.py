683ad75faa49e478a09b6f961b9816515da6d1d6
maurits <mauritsjpallesen@gmail.com>
2021-05-20 21:22:00 +0200

    can plot all files in folder in same figure for comparison

--- crop.py
+++ crop.py
@@ -3,11 +3,13 @@ from scipy.io import wavfile
 import copy
 import numpy as np
 import sys
+import os
 
 def show_crop(fp, thres, n):
     rate, data = wavfile.read(fp)
 
-    avg_data = data.sum(axis=1) // 2
+    avg_data = data.sum(axis=1) // 2 if wavDataIsStereo(data) else data
+    print(avg_data)
     print(avg_data.shape)
     sidx, eidx, cropped_data = crop_sound(avg_data, thres, n)
 
@@ -47,8 +49,38 @@ def crop_sound(data, threshold, n):
 
 	return start_idx, end_idx, data[start_idx:end_idx]
 
+def plot_all_audios_in_folder(folderPath, threshold, n):
+    files = os.listdir(folderPath)
+    plt.figure()
+    for i in range(len(files)):
+        rate, data = wavfile.read(folderPath + files[i])
+        avg_data = data.sum(axis=1) // 2 if wavDataIsStereo(data) else data
+
+        N = avg_data.shape[0]
+        s = N / rate
+        t_step = 1.0 / rate
+
+        t = np.arange(0, s, t_step)
+
+        if (t.shape != avg_data.shape):
+            continue
+
+        plt.plot(t, avg_data)
+
+    plt.show()
+
+def wavDataIsStereo(data):
+    return data.ndim == 2
+
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
+    if (len(sys.argv) != 4):
         print("Usage: cropt.py <FILE> <THRESHOLD> <N>")
+        print("   or: cropt.py <DIRECTORY> <THRESHOLD> <N>")
         sys.exit()
-    show_crop(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+    if (os.path.isfile(sys.argv[1])):
+        show_crop(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+    elif (os.path.isdir(sys.argv[1])):
+        plot_all_audios_in_folder(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+    else:
+        print(sys.argv[1] + " must be a file or a directory")
